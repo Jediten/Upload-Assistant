975b8486bc6127ca8c0c8eeec6b935136e0ce5eb
Jediten <Jeditwo@proton.me>
2026-07-05 21:56:45 -0400

    ZNTH: add banned release groups list

--- src/trackers/ZNTH.py
+++ src/trackers/ZNTH.py
@@ -19,4 +19,10 @@ class ZNTH(UNIT3D):
         self.search_url = f'{self.base_url}/api/torrents/filter'
         self.requests_url = f'{self.base_url}/api/requests/filter'
         self.torrent_url = f'{self.base_url}/torrents/'
-        self.banned_groups = ['']
+        self.banned_groups = [
+            '4K4U', 'Alcaide_Kira', 'AROMA', 'aXXo', 'BiTOR', 'BRrip', 'CM8', 'CrEwSaDe', 'd3g', 'DNL',
+            'EVO', 'FaNGDiNG0', 'FGT', 'FRDS', 'GalaxyTV', 'HD2DVD', 'HDTime', 'Hi10', 'ION10', 'iPlanet',
+            'KiNGDOM', 'LAMA', 'MeGusta', 'mHD', 'mSD', 'NhaNc3', 'nHD', 'nikt0', 'nSD', 'OFT', 'PRODJi',
+            'RARBG', 'SANTi', 'SPDVD', 'STUTTERSHIT', 'Telly', 'TGx', 'TSP', 'TSPxL', 'WAF', 'x0r',
+            'YIFY', 'YTS',
+        ]
